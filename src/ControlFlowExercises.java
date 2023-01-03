c63ea6d88cd012b121f28683f6557aa083672b98
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-01-03 15:58:55 -0600

    Control Flow

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,37 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        //#1
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //#2
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i+=2;
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i-=5;
+//        } while (i >= -10);
+
+        long i = 2;
+        do {
+            System.out.println(i);
+            i*=i;
+        } while (i <= 1000000);
+
+        //Above in for loops
+//        for(int i = 0; i < 10; i += 1) {
+//            System.out.println("i is " + i);
+//        }
+
+
+    }
+}
